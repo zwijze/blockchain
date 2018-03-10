474303ec56cc82e80b02643b909bc87b6c65bdcb
zwijze <frank.zwijze@gmail.com>
2018-03-10 22:24:02 +0100

    Update for adding block and mining

--- noobchain/src/noobchain/Block.java
+++ noobchain/src/noobchain/Block.java
@@ -2,23 +2,25 @@ package noobchain;
 
 import java.util.Date;
 
+
+
 public class Block {
 
-
+	
 
 	public String hash;
 
-	public String previousHash;
+	public String previousHash; 
 
 	private String data; //our data will be a simple message.
 
 	private long timeStamp; //as number of milliseconds since 1/1/1970.
+
+	private int nonce;
+
 	
-	//this.hash = calculateHash(); //Making sure we do this after we set the other values.
 
-
-
-	//Block Constructor.
+	//Block Constructor.  
 
 	public Block(String data,String previousHash ) {
 
@@ -28,12 +30,16 @@ public class Block {
 
 		this.timeStamp = new Date().getTime();
 
-		this.hash = calculateHash(); 
 		
+
+		this.hash = calculateHash(); //Making sure we do this after we set the other values.
+
 	}
+
 	
-	
-	
+
+	//Calculate new hash based on blocks contents
+
 	public String calculateHash() {
 
 		String calculatedhash = StringUtil.applySha256( 
@@ -42,11 +48,31 @@ public class Block {
 
 				Long.toString(timeStamp) +
 
+				Integer.toString(nonce) + 
+
 				data 
 
 				);
 
 		return calculatedhash;
+
+	}
+
+	
+
+	public void mineBlock(int difficulty) {
+
+		String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0" 
+
+		while(!hash.substring( 0, difficulty).equals(target)) {
+
+			nonce ++;
+
+			hash = calculateHash();
+
+		}
+
+		System.out.println("Block Mined!!! : " + hash);
 
 	}
 
