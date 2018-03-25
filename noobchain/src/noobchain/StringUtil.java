8d2af5c04b0f6f64f2bffe6a568174fa234aa7a5
zwijze <frank.zwijze@gmail.com>
2018-03-25 13:45:52 +0200

    Begin of setup transactions Signed-off-by: zwijze <frank.zwijze@gmail.com>

--- noobchain/src/noobchain/StringUtil.java
+++ noobchain/src/noobchain/StringUtil.java
@@ -1,6 +1,11 @@
 package noobchain;
 
+import java.security.Key;
 import java.security.MessageDigest;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.util.Base64;
 
 
 
@@ -40,6 +45,69 @@ public class StringUtil {
 
 		}
 
-	}	
+	}
+	
+	public static byte[] applyECDSASig(PrivateKey privateKey, String input) {
+
+	Signature dsa;
+
+	byte[] output = new byte[0];
+
+	try {
+
+		dsa = Signature.getInstance("ECDSA", "BC");
+
+		dsa.initSign(privateKey);
+
+		byte[] strByte = input.getBytes();
+
+		dsa.update(strByte);
+
+		byte[] realSig = dsa.sign();
+
+		output = realSig;
+
+	} catch (Exception e) {
+
+		throw new RuntimeException(e);
+
+	}
+
+	return output;
+
+}
+
+
+
+//Verifies a String signature 
+
+public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature) {
+
+	try {
+
+		Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+
+		ecdsaVerify.initVerify(publicKey);
+
+		ecdsaVerify.update(data.getBytes());
+
+		return ecdsaVerify.verify(signature);
+
+	}catch(Exception e) {
+
+		throw new RuntimeException(e);
+
+	}
+
+}
+
+
+
+public static String getStringFromKey(Key key) {
+
+	return Base64.getEncoder().encodeToString(key.getEncoded());
+
+}
+
 
 }
