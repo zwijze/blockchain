f5bda5f53321511389ca3699e3331943da66afe9
zwijze <frank.zwijze@gmail.com>
2018-03-27 22:02:48 +0200

    Tranactions setup Signed-off-by: zwijze <frank.zwijze@gmail.com>

--- noobchain/src/noobchain/Noobchain.java
+++ noobchain/src/noobchain/Noobchain.java
@@ -1,7 +1,12 @@
 package noobchain;
 
 
+
+import java.security.Security;
+
 import java.util.ArrayList;
+
+import java.util.Base64;
 
 import com.google.gson.GsonBuilder;
 
@@ -15,14 +20,45 @@ public class Noobchain {
 
 	public static int difficulty = 5;
 
+	public static Wallet walletA;
+
+	public static Wallet walletB;
+
 
 
 	public static void main(String[] args) {	
 
-		//add our blocks to the blockchain ArrayList:
+		//Setup Bouncey castle as a Security Provider
 
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider()); 
+
+		//Create the new wallets
+
+		walletA = new Wallet();
+
+		walletB = new Wallet();
+
+		//Test public and private keys
+
+		System.out.println("Private and public keys:");
+
+		System.out.println(StringUtil.getStringFromKey(walletA.privateKey));
+
+		System.out.println(StringUtil.getStringFromKey(walletA.publicKey));
+
+		//Create a test transaction from WalletA to walletB 
+
+		Transaction transaction = new Transaction(walletA.publicKey, walletB.publicKey, 5, null);
+
+		transaction.generateSignature(walletA.privateKey);
+
+		//Verify the signature works and verify it from the public key
+
+		System.out.println("Is signature verified");
+
+		System.out.println(transaction.verifiySignature());
 		
-
+		//add our blocks to the blockchain ArrayList:
 		blockchain.add(new Block("Hi im the first block", "0"));
 
 		System.out.println("Trying to Mine block 1... ");
--- noobchain/src/noobchain/TransactionInput.java
+++ noobchain/src/noobchain/TransactionInput.java
@@ -2,4 +2,16 @@ package noobchain;
 
 public class TransactionInput {
 
+	public String transactionOutputId; //Reference to TransactionOutputs -> transactionId
+
+	public TransactionOutput UTXO; //Contains the Unspent transaction output
+
+	
+
+	public TransactionInput(String transactionOutputId) {
+
+		this.transactionOutputId = transactionOutputId;
+
+	}
+
 }
